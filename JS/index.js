05d56ab9bf693eba2e6e1580739951a51ac3c582
Mahmoud Mortada <mahmoudtaha654@gmail.com>
2023-02-12 17:38:46 +0200

    updated on Assignment 8 and change it to Assignment 9 (BookMarker)

--- JS/index.js
+++ JS/index.js
@@ -1,196 +1,85 @@
-var productName = document.getElementById("product-name");
-var productPrice = document.getElementById("product-price");
-var productCategory = document.getElementById("product-cat");
-var productDescription = document.getElementById("product-desc");
-var productList = [];
+var siteName = document.querySelector("#site-name");
+var siteURL = document.querySelector("#site-url");
+var submitBtn = document.querySelector("#submitBtn");
+var bookMarkList = [];
+var clearBtn;
+var siteNameErrorMsg = document.querySelector("#site-name-error");
+var siteURLErrorMsg = document.querySelector("#website-url-error");
+console.log(siteName);
+console.log(siteURL);
 
-var ProductListNameinLocalStorage = "Products";
-
-if (!getProductsFromLocalStorage()) {
-  productList = [];
-} else {
-  productList = getProductsFromLocalStorage();
-  displayProduct(productList);
-}
-
-// Create New Product and push it to the ProductList
-function addProduct() {
-  if (
-    productNameValidation() &&
-    productPriceValidation() &&
-    productCatValidation() &&
-    productDescValidation()
-  ) {
-    var product = {
-      name: productName.value,
-      price: productPrice.value,
-      category: productCategory.value,
-      description: productDescription.value,
+submitBtn.addEventListener("click", function (event) {
+  if (validateSiteName() && validatesiteURL()) {
+    var bookmark = {
+      name: siteName.value,
+      url: siteURL.value,
     };
-    productList.push(product);
-    displayProduct(productList);
-    setProductsinLocalStorage(productList);
-    updateProductsInputsWithNewValues();
-  }
-}
-
-function displayProduct(productList) {
-  var cartona = ``;
-  for (var i = 0; i < productList.length; i++) {
-    cartona += ` <tr>
-    <td>${i + 1}</td>
-    <td>${
-      productList[i].newName ? productList[i].newName : productList[i].name
-    }</td>
-    <td>${productList[i].price}</td>
-    <td>${productList[i].category}</td>
-    <td>${productList[i].description}</td>
-    <td>
-      <button class="btn btn-success btn-sm" onclick="editproduct(${i})">
-        Edit
-      </button>
-    </td>
-    <td>
-      <button class="btn btn-danger btn-sm" onclick="deleteProduct(${i})">
-        Delete
-      </button>
-    </td>
-  </tr>`;
-  }
-  document.getElementById("product-data").innerHTML = cartona;
-}
-
-function deleteProduct(index) {
-  productList.splice(index, 1);
-  displayProduct(productList);
-  setProductsinLocalStorage(productList);
-}
-
-function editproduct(index) {
-  document.getElementById("search").value = "";
-  // displayProduct(productList);
-  document.getElementById("addProductBtn").classList.add("d-none");
-  document.getElementById("updateProductBtn").classList.remove("d-none");
-
-  updateProductsInputsWithNewValues(productList[index]);
-
-  document.getElementById("updateProductBtn").onclick =
-    function updateProduct() {
-      if (
-        productNameValidation() &&
-        productPriceValidation() &&
-        productCatValidation() &&
-        productDescValidation()
-      ) {
-        productList[index].name = productName.value;
-        productList[index].price = productPrice.value;
-        productList[index].category = productCategory.value;
-        productList[index].description = productDescription.value;
-        productList[index].newName = "";
-
-        displayProduct(productList);
-
-        updateProductsInputsWithNewValues();
-        setProductsinLocalStorage(productList);
-        document.getElementById("addProductBtn").classList.remove("d-none");
-        document.getElementById("updateProductBtn").classList.add("d-none");
-      }
-    };
-}
-
-var searchProducts = function (term) {
-  var foundedProductsinSearch = [];
-
-  for (var i = 0; i < productList.length; i++) {
-    if (productList[i].name.toLowerCase().includes(term.toLowerCase())) {
-      productList[i].newName = productList[i].name
-        .toLowerCase()
-        .replace(
-          term.toLowerCase(),
-          `<span class="text-danger bg-warning">${term}</span>`
-        );
-      foundedProductsinSearch.push(productList[i]);
-    }
-  }
-  displayProduct(foundedProductsinSearch);
-};
-
-function updateProductsInputsWithNewValues(flag) {
-  productName.value = flag ? flag.name : "";
-  productPrice.value = flag ? flag.price : "";
-  productCategory.value = flag ? flag.category : "";
-  productDescription.value = flag ? flag.description : "";
-}
-
-function setProductsinLocalStorage() {
-  localStorage.setItem("Products", JSON.stringify(productList));
-}
-
-function getProductsFromLocalStorage() {
-  return JSON.parse(localStorage.getItem(ProductListNameinLocalStorage))
-    ? JSON.parse(localStorage.getItem(ProductListNameinLocalStorage))
-    : null;
-}
-
-function productNameValidation() {
-  var regex = /^[A-Z][a-z]{3,8}$/;
-  if (regex.test(productName.value)) {
-    document
-      .getElementById("product-name-error")
-      .classList.replace("d-block", "d-none");
-    document.getElementById("product-name").style.border = "1px solid blue";
+    bookMarkList.push(bookmark);
+    displayBookmarks(bookMarkList);
+    clearInputs();
+  } else if (!validateSiteName() && validatesiteURL()) {
+    siteNameErrorMsg.classList.replace("d-none", "d-block");
+  } else if (!validatesiteURL() && validateSiteName()) {
+    siteURLErrorMsg.classList.replace("d-none", "d-block");
   } else {
-    document
-      .getElementById("product-name-error")
-      .classList.replace("d-none", "d-block");
-    document.getElementById("product-name").style.border = "1px solid red";
+    siteNameErrorMsg.classList.replace("d-none", "d-block");
+    siteURLErrorMsg.classList.replace("d-none", "d-block");
   }
+});
+siteName.addEventListener("change", function () {
+  if (!validateSiteName()) {
+    siteNameErrorMsg.classList.replace("d-none", "d-block");
+  } else {
+    siteNameErrorMsg.classList.replace("d-block", "d-none");
+  }
+});
 
-  return regex.test(productName.value);
-}
-function productPriceValidation() {
-  var regex = /^([1-9][0-9][0-9][0-9]|10000)$/;
-  if (regex.test(productPrice.value)) {
-    document
-      .getElementById("product-price-error")
-      .classList.replace("d-block", "d-none");
-    document.getElementById("product-price").style.border = "1px solid blue";
+siteURL.addEventListener("change", function () {
+  if (!validatesiteURL()) {
+    siteURLErrorMsg.classList.replace("d-none", "d-block");
   } else {
-    document
-      .getElementById("product-price-error")
-      .classList.replace("d-none", "d-block");
-    document.getElementById("product-price").style.border = "1px solid red";
+    siteURLErrorMsg.classList.replace("d-block", "d-none");
   }
-  return regex.test(productPrice.value);
+});
+
+function displayBookmarks(List) {
+  cartona = "";
+  for (var i = 0; i < List.length; i++) {
+    cartona += ` <div
+    class="bookmark-item d-flex justify-content-between align-items-center p-4 mb-4"
+  >
+    <h4 class="text-primary">${List[i].name}</h4>
+    <div class="pe-4">
+      <a href="https://${List[i].url}" class="btn btn-success" target="_blank">Visit</a>
+      <button class="btn btn-danger mx-auto clearBtn" >Clear</button>
+    </div>
+  </div>`;
+  }
+  document.querySelector("#bookmarks").innerHTML = cartona;
+
+  clearBtn = Array.from(document.querySelectorAll(".clearBtn"));
+  console.log(clearBtn);
+
+  for (let index = 0; index < clearBtn.length; index++) {
+    clearBtn[index].addEventListener("click", function () {
+      bookMarkList.splice(index, 1);
+      displayBookmarks(bookMarkList);
+    });
+  }
 }
 
-function productCatValidation() {
-  var regex = /^(TV|Mobile|Watch)$/;
-  if (regex.test(productCategory.value)) {
-    document
-      .getElementById("product-cat-error")
-      .classList.replace("d-block", "d-none");
-    document.getElementById("product-cat").style.border = "1px solid blue";
-  } else {
-    document
-      .getElementById("product-cat-error")
-      .classList.replace("d-none", "d-block");
-    document.getElementById("product-cat").style.border = "1px solid red";
-  }
-  return regex.test(productCategory.value);
+function validateSiteName() {
+  var regex = /^[A-Z][a-z]{2,}$/;
+  return regex.test(siteName.value);
 }
-function productDescValidation() {
-  var regex = /^(.){0,250}$/;
-  if (regex.test(productDescription.value)) {
-    document
-      .getElementById("product-desc-error")
-      .classList.replace("d-block", "d-none");
-    document.getElementById("product-desc").style.border = "1px solid blue";
-  } else {
-    document
-      .getElementById("product-desc-error")
-      .classList.replace("d-none", "d-block");
-    document.getElementById("product-desc").style.border = "1px solid red";
-  }
-  return regex.test(productDescription.value);
+
+function validatesiteURL() {
+  var regex =
+    /^[(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+  return regex.test(siteURL.value);
+}
+
+function clearInputs() {
+  siteName.value = "";
+  siteURL.value = "";
 }
